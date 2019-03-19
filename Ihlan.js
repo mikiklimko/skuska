4714973188ad4b744c9d5ac1207c005b6adb7c88
mikiklimko <miki.klimko@gmail.com>
2019-03-19 15:12:32 +0100

    ihlan

--- Ihlan.js
+++ Ihlan.js
@@ -11,7 +11,7 @@ camera.position.y = 0;
 camera.position.x = 0;
 
 
-var gui = new dat.GUI();
+
 var speed = 0.1;
 parameters =
     {
@@ -25,3 +25,67 @@ parameters =
         
 
     };
+
+   
+    var typ =   typ.onChange(function (geom) {
+    if (geom === "Kocka") {
+        cube.visible = true;
+        cone.visible = false;
+        cylinder.visible = false;
+    }
+    if (geom === "Ihlan") {
+        cube.visible = false;
+        cone.visible = true;
+        cylinder.visible = false;
+    }
+    if (geom === "Valec"){
+        cube.visible = false;
+        cone.visible = false;
+        cylinder.visible = true;
+    }
+    console.log(geom)
+
+});
+ 
+
+
+var color = color.onChange(function (color) { cone.material.color.setHex(color.replace("#", "0x")); });
+
+
+
+xdimen.onChange(function (pohyb) { cone.scale.x = pohyb; });
+ydimen.onChange(function (pohyb) { cone.scale.y = pohyb; });
+zdimen.onChange(function (pohyb) { cone.scale.z = pohyb; });
+
+
+
+
+xpos.onChange(function (pozicia) { cone.position.x = pozicia });
+ypos.onChange(function (pozicia) { cone.position.y = pozicia });
+zpos.onChange(function (pozicia) { cone.position.z = pozicia });
+
+
+
+
+xanim.onChange(function (anim) { xro = anim; console.log(anim) } );
+yanim.onChange(function (anim) { yro = anim; });
+zanim.onChange(function (anim) { zro = anim; });
+
+
+var obj = { restart:function(){ 
+    camera.position.z = 3;
+    cube.scale.x = 1 ;
+    cube.scale.z = 1 ;
+    cube.scale.y = 1 ;
+    cube.position.x = 0;
+    cube.position.y = 0;
+    cube.position.z = 0;
+    
+
+ }};
+
+    
+
+
+
+gui.open();
